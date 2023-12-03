5c55b77e52d0ea983dbf832fb986aa7280756dc9
Asma Draouil <asmadraouil2@gmail.com>
2023-12-03 17:09:44 +0100

    test

---
+++ src/test/java/com/projet/subject/SubjectMapperTest.java
@@ -0,0 +1,32 @@
+package com.projet.subject;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SubjectMapperTest {
+
+private final SubjectMapper mapper = new SubjectMapper();
+@Test
+public void should_successfully_transform_subject_request_to_Subject() {
+    SubjectRequest request = new SubjectRequest();
+    request.setId(null);
+    request.setName("Math");
+    
+    Subject subject = mapper.toSubject(request);
+
+    Assertions.assertNull(subject.getId());
+    Assertions.assertEquals( "Math", subject.getName());
+    Assertions.assertNull(subject.getStudents());
+    Assertions.assertNull(subject.getChapters());
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
