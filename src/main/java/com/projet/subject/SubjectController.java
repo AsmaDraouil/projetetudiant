348c6d1207199bfa7b1af72d2a07cb90addf7ca9
Asma Draouil <asmadraouil2@gmail.com>
2023-12-13 23:57:29 +0100

    assign method

--- src/main/java/com/projet/subject/SubjectController.java
+++ src/main/java/com/projet/subject/SubjectController.java
@@ -2,12 +2,16 @@ package com.projet.subject;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -19,20 +23,33 @@ private final SubjectService service;
 	
 	
 @PostMapping
-public void save (@RequestBody SubjectRequest subject) {
+public ResponseEntity<Void> save (@RequestBody @Valid SubjectRequest subject) {
 	service.save(subject);
+	return ResponseEntity
+			.accepted()
+			.build();
 }
 	
 @GetMapping("/{subjectId}")
-public SubjectResponse findById(@PathVariable  Integer subjectId) {
-return service.findById(subjectId);
+public ResponseEntity<SubjectResponse> findById(@PathVariable  Integer subjectId) {
+return ResponseEntity.ok(service.findById(subjectId));
 }	
 
 @GetMapping
-public List<SubjectResponse> findAll(){
-	return service.findAll();
+public ResponseEntity<List<SubjectResponse>> findAll(){
+	return ResponseEntity.ok(service.findAll());
 }
 
+@PatchMapping("/{subjectId}/student/{studentId}")
+public ResponseEntity<Void> assignSubjectToStudent(
+		@PathVariable Integer subjectId,
+		@PathVariable Integer studentId
+		){
 
-
+	service.assignSubjectToStudent(subjectId, studentId);
+	return ResponseEntity
+			.accepted()
+			.build();
+	
+}
 }
